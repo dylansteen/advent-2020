8bd1f6f4e6a15d8dc1f82fd39fc723236827d4cc
dylansteen <dsteen@petalmd.com>
2020-12-03 21:47:09 -0500

    Day 3 but using functional programming and shorter

--- 3-1.js
+++ 3-1.js
@@ -8,30 +8,19 @@ function getMap() {
 
 async function main() {
   const positionMap = await getMap();
-  let position = [0, 0];
   const maxX = positionMap[0].length;
-  const maxY = positionMap.length - 1 ;
 
-  let collisions = 0;
-  while(position[1] <= maxY) {
-    if (hasCollision(position, positionMap)) {
-      collisions++
-    }
-    position = move(position, 3, 1, maxX);
-  }
-  
+  let xPos = 0;
+
+  const collisions = positionMap.reduce((acc, row) => {
+    const result = hasCollision(xPos, row) ? acc + 1 : acc;
+    xPos = move(xPos, 3, maxX);
+    return result;
+  }, 0);
   console.log(collisions);
 }
 
-const move = (previous, x, y, maxX) => {
-  const [prevX, prevY] = previous;
-  return [(prevX + x) % maxX, prevY + y];
-}
-
-const hasCollision = (position, input) => {
-  const [xPos, yPos] = position;
-
-  return input[yPos][xPos] === '#';
-}
+const move = (previous, x, maxX) =>  (previous + x) % maxX;
+const hasCollision = (xPos, row) => row[xPos] === '#';
 
 main();
--- 3-2.js
+++ 3-2.js
@@ -9,33 +9,26 @@ function getMap() {
 async function main() {
   const positionMap = await getMap();
   const maxX = positionMap[0].length;
-  const maxY = positionMap.length - 1 ;
   const slopes = [[1, 1], [3, 1], [5, 1], [7, 1], [1, 2]];
 
   const collisionsArr = slopes.map((slope) => {
-    let position = [0, 0];
-    let collisions = 0;
-    while(position[1] <= maxY) {
-      if (hasCollision(position, positionMap)) {
-        collisions++
-      }
-      position = move(position, slope[0], slope[1], maxX);
-    }
-    return collisions
+    let xPos = 0;
+    const resultingMap = positionMap.filter((row, index) => !(index % slope[1]));
+    const deltaX = slope[0];
+
+    const collisions = resultingMap.reduce((acc, row) => {
+      const result = hasCollision(xPos, row) ? acc + 1 : acc;
+      xPos = move(xPos, deltaX, maxX);
+      return result;
+    }, 0);
+
+    return collisions;
   });
   
   console.log(collisionsArr.reduce((acc, curr) => acc * curr));
 }
 
-const move = (previous, x, y, maxX) => {
-  const [prevX, prevY] = previous;
-  return [(prevX + x) % maxX, prevY + y];
-}
-
-const hasCollision = (position, input) => {
-  const [xPos, yPos] = position;
-
-  return input[yPos][xPos] === '#';
-}
+const move = (previous, x, maxX) => (previous + x) % maxX;
+const hasCollision = (xPos, row) => row[xPos] === '#';
 
 main();
